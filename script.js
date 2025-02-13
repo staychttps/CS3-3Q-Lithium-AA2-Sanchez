64e7d6ed7e6387c7fe3887ae3b943fda6f295fb0
stayc <sicsanchez22@src.pshs.edu.ph>
2025-02-14 07:45:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function totalEnemies(){
+	let P0 = parseFloat((prompt("Enter the initial population");
+	let r = parseFloat((prompt("Enter the rate of growth"));
+	let t = parseFloat((prompt("Enter the time(in hours)"));
+	
+	let finalPopulation=Math.round(P0*Math.pow(Math.E,r*t));
+	
+	let region = (prompt("Enter a region"));
+	let nameEnemy =(prompt("Enter the enemies' name"));
+	
+	let enemyInfo = (region + ' ' + nameEnemy).toUpperCase;
+	
+	document.getElementById("result").innerHTML = 'After' + t + 'hours, the population of ' + region + nameEnemy + 'has increased by' + finalPopulation '!';
+	
+	
+	
