34600f85670dcd80ccc02d42016477e3f477dd47
stayc <sicsanchez22@src.pshs.edu.ph>
2025-02-15 23:40:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function totalEnemies(){
 
 	let P0 = parseFloat(prompt("Enter the initial population"));
-	let r = parseFloat(prompt("Enter the rate of growth(in decimal form)"));
+	let r = parseFloat(prompt("Enter the rate of growth(range from zero to one)"));
 	let t = parseFloat(prompt("Enter the time(in hours)"));
 	
-	let finalPopulation=Math.round(P0*Math.pow(Math.E,r*t));
+	let finalPopulation = Math.round(P0 * Math.pow(Math.E, r, t));
 	
 	let region = prompt("Enter a region");
 	let nameEnemy =prompt("Enter the enemies' name");
 	
 	let enemyInfo = region.concat(' ', nameEnemy).toUpperCase;
 	 
-	document.getElementById("result").innerHTML = "After " + t + " hours, the population of " + enemyInfo + "has increased by" + finalPopulation + "!!";
+	document.getElementById("result").innerHTML = "After ".concat(t, " hours, the population of ", enemyInfo , " has increased by ", finalPopulation, "!!");
 }
